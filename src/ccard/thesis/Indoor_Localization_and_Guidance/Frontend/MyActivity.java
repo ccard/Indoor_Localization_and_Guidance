c93950867f2e3deea665122607f9a1abfb6e6602
ccard <ccard@mymail.mines.edu>
2014-03-03 10:54:28 -0700

    update permissions and imports

--- src/ccard/thesis/Indoor_Localization_and_Guidance/Frontend/MyActivity.java
+++ src/ccard/thesis/Indoor_Localization_and_Guidance/Frontend/MyActivity.java
@@ -3,8 +3,15 @@ package ccard.thesis.Indoor_Localization_and_Guidance.Frontend;
 import android.app.Activity;
 import android.os.Bundle;
 import ccard.thesis.Indoor_Localization_and_Guidance.R;
+import org.opencv.*;
+import org.opencv.android.OpenCVLoader;
 
 public class MyActivity extends Activity {
+    static{
+        if(!OpenCVLoader.initDebug()){
+        //TODO: error reporting
+        }
+    }
     /**
      * Called when the activity is first created.
      */

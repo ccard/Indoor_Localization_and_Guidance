95f052ba24247e81e498d597c68a31381c05a3d6
ccard <ccard@mymail.mines.edu>
2014-03-03 10:53:46 -0700

    added mk files for ndk

---
+++ Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+OPENCV_CAMERA_MODULES:=on
+OPENCV_INSTALL_MODULES:=on
+OPENCV_LIB_TYPE:=STATIC
+include C:\NVPACK\OpenCV-2.4.5-Tegra-sdk-r2\sdk\native\jni\OpenCV.mk

2f15993df14bc0c01a602d5569beee69a808859b
ccard <ccard@mymail.mines.edu>
2014-03-03 13:26:15 -0700

    add interfaces for images and image providors

---
+++ src/ccard/thesis/Indoor_Localization_and_Guidance/Backend/ImageContainer.java
@@ -0,0 +1,84 @@
+package ccard.thesis.Indoor_Localization_and_Guidance.Backend;
+
+import android.view.View;
+import org.json.JSONObject;
+import org.opencv.core.Mat;
+import org.opencv.features2d.DMatch;
+import org.opencv.features2d.KeyPoint;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Chris Card on 3/3/14.
+ * This interface is for images
+ */
+public interface ImageContainer{
+
+    public enum MaskTypes{Circle,Square}
+    /**
+     * Retreives a KeyPoint from an index with in the image
+     * @param index of the keypoint
+     * @return the keypoint
+     */
+    public KeyPoint getKeyPoint(int index);
+
+    /**
+     * This returns all the key points for the image
+     * @return a list of all the key points
+     */
+    public ArrayList<KeyPoint> getKeyPoints();
+
+    /**
+     * This gets the mat descriptor of the image
+     * @return the descriptor of the image
+     */
+    public Mat getDescriptor();
+
+    /**
+     * This method tells the user if they can draw the image onto
+     * a view
+     * @return true if the image can be drawn
+     */
+    public boolean hasImageToDraw();
+
+    /**
+     * This method draws the image into a view
+     * @param view the view to draw the image into
+     * @param withKeyPoints if key Points are to be drawn
+     * @return true if it succeded
+     */
+    public boolean render(View view,boolean withKeyPoints);
+
+    /**
+     * This method draws the comparison between the two image keypoints
+     * @param view the view to draw the image on
+     * @param im2 the image that it will be compared to
+     * @param matches the list of matches between the two images
+     * @return true if it succeeded
+     */
+    public boolean renderComparision(View view,ImageContainer im2,
+                                     ArrayList<DMatch> matches);
+
+    /**
+     * This method makes a mask
+     * @param type the type of the mask that should be created
+     * @param radius_side if the lenght of the list is 1 then it is a
+     *                    radius if its length is two then it is a square
+     * @return true if it succeeded
+     */
+    public boolean makeMask(MaskTypes type, int... radius_side);
+
+    /**
+     * This method inits the descripotr based on the params passed in
+     * @param params This requires a fields being the type of descriptor
+     * @return true if it succeeded
+     */
+    public boolean initDescriptor(JSONObject params);
+
+    /**
+     * This method calculates the descriptor for the image
+     * @return true if it succeeded
+     */
+    public boolean calculateDescriptor();
+
+}
---
+++ src/ccard/thesis/Indoor_Localization_and_Guidance/Backend/ImageProvidor.java
@@ -0,0 +1,40 @@
+package ccard.thesis.Indoor_Localization_and_Guidance.Backend;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Chris Card on 3/3/14.
+ * This is an interface for the Image Providor and will get
+ * provides images from a database or a server etc..
+ */
+public interface ImageProvidor {
+
+    /**
+     * This method sets the intentservice to use for when retrieving data
+     * @param intentService the class of the intent service to use when retreivng
+     *                      images
+     */
+    public void setIntentService(Class<?> intentService);
+
+    /**
+     * Retrieves the image at the desired index
+     * @param index of the image in the database
+     * @return the image container of the image at the index
+     */
+    public ImageContainer getImage(int index);
+
+    /**
+     * This methods gets all images that the image provider has
+     * @return the list of images
+     */
+    public ArrayList<ImageContainer> getImages();
+
+    /**
+     * This method requests
+     * @param params
+     */
+    public void requestImages(JSONObject params);
+
+}

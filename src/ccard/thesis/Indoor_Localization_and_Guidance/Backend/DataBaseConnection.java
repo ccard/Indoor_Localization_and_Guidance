0621c191f188befb6937aa63dda8698fa32079bd
ccard <ccard@mymail.mines.edu>
2014-03-03 13:26:49 -0700

    add database connection service

---
+++ src/ccard/thesis/Indoor_Localization_and_Guidance/Backend/DataBaseConnection.java
@@ -0,0 +1,14 @@
+package ccard.thesis.Indoor_Localization_and_Guidance.Backend;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by Ch on 3/3/14.
+ */
+public class DataBaseConnection extends Service {
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}

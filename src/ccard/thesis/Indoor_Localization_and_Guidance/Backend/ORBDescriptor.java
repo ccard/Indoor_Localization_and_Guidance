f522ed9fe53a2a4965dc35fbd854365036509afe
ccard <ccard@mymail.mines.edu>
2014-03-04 09:55:33 -0700

    added xml parser to convert json to xml. started adding code to orbdescriptor

--- src/ccard/thesis/Indoor_Localization_and_Guidance/Backend/ORBDescriptor.java
+++ src/ccard/thesis/Indoor_Localization_and_Guidance/Backend/ORBDescriptor.java
@@ -1,9 +1,11 @@
 package ccard.thesis.Indoor_Localization_and_Guidance.Backend;
 
 import org.json.JSONObject;
-import org.opencv.core.Mat;
+import org.opencv.core.*;
+import org.opencv.features2d.DescriptorExtractor;
 import org.opencv.features2d.FeatureDetector;
 import org.opencv.features2d.KeyPoint;
+
 
 import java.util.ArrayList;
 
@@ -15,28 +17,57 @@ import java.util.ArrayList;
 public class ORBDescriptor implements Descriptor {
 
     FeatureDetector detect;
+    DescriptorExtractor des;
+
+    Mat descript,mask;
+    ArrayList<KeyPoint> keyPoints;
 
     public ORBDescriptor(){
-        detect = FeatureDetector.create(FeatureDetector.ORB);
+        des = DescriptorExtractor.create(DescriptorExtractor.ORB);
+        keyPoints = new ArrayList<KeyPoint>();
     }
 
     @Override
     public ArrayList<KeyPoint> getKeyPoints() {
-        return null;
+        return keyPoints;
     }
 
     @Override
     public Mat getDescriptor() {
-        return null;
+        return descript;
     }
 
     @Override
     public boolean makeMask(MaskTypes type, int... radius_side) {
-        return false;
+        if(radius_side.length > 2 || radius_side.length == 0) return false;
+
+        int radius_s,side2 = 0;
+        if (radius_side.length == 1){
+            radius_s = radius_side[0];
+        } else {
+            radius_s = radius_side[0];
+            side2 = radius_side[1];
+        }
+
+        switch (type){
+            case Circle:
+                mask = Mat.zeros(2*radius_s,2*radius_s, CvType.CV_8U);
+                Core.circle(mask,new Point(radius_s,radius_s),radius_s,
+                        new Scalar(1,1,1),-1);
+                break;
+            case Square:
+                mask = Mat.zeros(radius_s,side2,CvType.CV_8U);
+                Core.rectangle(mask,new Point(0,0),new Point(radius_s,side2),
+                        new Scalar(1,1,1),-1);
+                break;
+        }
+        return true;
     }
 
     @Override
     public boolean initDescriptor(JSONObject params) {
+        detect = FeatureDetector.create(FeatureDetector.ORB);
+
         return false;
     }
 
---
+++ src/ccard/thesis/Indoor_Localization_and_Guidance/Backend/XMLparser.java
@@ -0,0 +1,51 @@
+package ccard.thesis.Indoor_Localization_and_Guidance.Backend;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+/**
+ * Created by Chris Card on 3/4/14.
+ * This class contains methods for iterating through json objects and
+ * making them into xml
+ */
+public class XMLparser {
+
+    static final String xml_header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+
+    public static String build_XML(JSONObject j){
+        StringBuilder xml = new StringBuilder();
+        xml.append(xml_header+"\n");
+
+
+
+        return xml.toString();
+    }
+
+    private static String build_helper(JSONObject j){
+        StringBuilder xml = new StringBuilder();
+
+        Iterator<String> keys = j.keys();
+        while (keys.hasNext()){
+            String key = keys.next();
+            String val = null;
+            try {
+                JSONObject value = j.getJSONObject(key);
+                xml.append(build_helper(value));
+            } catch (Exception e) {
+                try {
+                    Object temp = j.get(key);
+                    val = temp.toString();
+                } catch (JSONException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+            if (val != null) {
+                xml.append("<"+key+">"+val+"</"+key+">\n");
+            }
+
+        }
+    }
+}

8ba5405f8649369878192b572cea1adae532d9e6
ccard <ccard@mymail.mines.edu>
2014-03-19 08:34:49 -0600

    add guidance layout for showing the images

---
+++ src/ccard/thesis/Indoor_Localization_and_Guidance/Frontend/Guidance.java
@@ -0,0 +1,14 @@
+package ccard.thesis.Indoor_Localization_and_Guidance.Frontend;
+
+import android.app.Activity;
+import android.os.Bundle;import ccard.thesis.Indoor_Localization_and_Guidance.R;
+
+/**
+ * Created by Ch on 3/19/14.
+ */
+public class Guidance extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.guidance_view);
+    }
+}

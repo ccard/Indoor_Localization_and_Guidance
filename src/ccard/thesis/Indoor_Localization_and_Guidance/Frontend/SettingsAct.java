0b5eab778b247901b1bc787c43df936ae7f01853
ccard <ccard@mymail.mines.edu>
2014-03-26 10:18:07 -0600

    add settings activity

---
+++ src/ccard/thesis/Indoor_Localization_and_Guidance/Frontend/SettingsAct.java
@@ -0,0 +1,14 @@
+package ccard.thesis.Indoor_Localization_and_Guidance.Frontend;
+
+import android.app.Activity;
+import android.os.Bundle;import ccard.thesis.Indoor_Localization_and_Guidance.R;
+
+/**
+ * Created by Ch on 3/26/14.
+ */
+public class SettingsAct extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_menu);
+    }
+}

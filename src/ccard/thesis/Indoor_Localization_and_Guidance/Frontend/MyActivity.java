cb04efb975ae278ecf1aa0ef15487418ddd55c49
ccard <ccard@mymail.mines.edu>
2014-04-21 11:20:44 -0600

    it is now connecting to google drive

--- src/ccard/thesis/Indoor_Localization_and_Guidance/Frontend/MyActivity.java
+++ src/ccard/thesis/Indoor_Localization_and_Guidance/Frontend/MyActivity.java
@@ -4,19 +4,28 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import ccard.thesis.Indoor_Localization_and_Guidance.Backend.Classes.LogFile;
 import ccard.thesis.Indoor_Localization_and_Guidance.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.OpenCVLoader;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
 
-    protected static final int REQUEST_CODE_RESOLUTION = 1;
+    private static final String TAG = "Drive fail";
+    private static final int REQUEST_CODE_UPLOAD = 1;
+    private static final int REQUEST_CODE_RESOLUTION = 2;
+    private GoogleApiClient apiClient;
     private BaseLoaderCallback mLoader = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -43,6 +52,27 @@ public class MyActivity extends Activity {
     }
 
     @Override
+    protected void onResume(){
+        super.onResume();
+        if(apiClient == null){
+            apiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        if (apiClient != null && apiClient.isConnected()){
+            apiClient.disconnect();
+        }
+        super.onPause();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inf = getMenuInflater();
         inf.inflate(R.menu.settings,menu);
@@ -56,6 +86,11 @@ public class MyActivity extends Activity {
                 Intent i = new Intent(this,SettingsDialog.class);
                 startActivity(i);
                 return true;
+            case R.id.upload:
+                if (!apiClient.isConnected()){
+                    apiClient.connect();
+                }
+                return true;
         }
         return true;
     }
@@ -63,5 +98,30 @@ public class MyActivity extends Activity {
     public void guide(View view){
         Intent i = new Intent(this,Guidance.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+         Log.i(TAG, "API client failed: "+connectionResult.toString());
+        if (!connectionResult.hasResolution()){
+            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(),this,0).show();
+            return;
+        }
+
+        try {
+            connectionResult.startResolutionForResult(this,REQUEST_CODE_RESOLUTION);
+        } catch (IntentSender.SendIntentException e) {
+            Log.e(TAG, "Exception while  starting resolution", e);
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG,"API Connected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 }
